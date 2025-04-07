cf2768ea03759b9fedc511d4bc42770bd3e2e867
matt-r-johns <mattjohns92@gmail.com>
2025-04-07 20:53:53 +0100

    Update urls.js

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,4 +1,4 @@
-const githubUsername = "vinaysomawat";
+const githubUsername = "matt-r-johns";
 const mediumUsername = "vinaysomawat";
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
