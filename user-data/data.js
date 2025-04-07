65b46015ac7048318812afb6c87f90c8d2989f14
matt-r-johns <mattjohns92@gmail.com>
2025-04-07 20:32:54 +0100

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -4,164 +4,76 @@ export const bio = [
 ];
 
 export const skills = [
-  "JavaScript, TypeScript, Angular, AngularJS, React, HTML5, CSS, Bootstrap, Tailwind CSS",
-  "RxJS, NGXS, NGRX, WebSockets, Stencil, Storybook, Module Federation, Webpack",
-  "Jenkins, JIRA, Git",
-  "Performance Optimization, Responsive Design, Cross-Browser Compatibility",
+  "Highly proficient cnc programming using solidcam, mastercam
+  "SolidWorks",
+  "Soldering, 3D Printing, python programming, excel+VBA",
 ];
 
 export const experience = [
   {
-    title: "Shiprocket Private Limited",
-    duration: "September 2023 - Present",
-    subtitle: "Software Engineer",
+    title: "SolidCAM UK",
+    duration: "April 2024 - Present",
+    subtitle: "Applications Engineer",
     details: [
-      "Developed a design system using Stencil and Storybook that provides consistent UI components across platforms, ensuring a cohesive design.",
-      "Developed a dynamic support ticket panel that renders subcategories based on component IDs, handling up to 100 AWBs simultaneously.",
-      "Migrated 30+ modules from AngularJS to Angular 15 using Generative AI tools, reporting directly to the VP of Engineering.",
-      "Designed and implemented advanced features such as keyboard shortcuts, improving operational efficiency by 15 percent.",
-      "Took ownership of OKRs, collaborated with the product team, and mentored two junior developers, cutting their code review iterations and ensuring on-time feature delivery.",
+      "Training, Post Processor writing and modification, on-site applications support, create and maintain training materials, build systems.",
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "AngularJs"],
     icon: "truck ",
   },
   {
-    title: "Biofourmis India Private Limited",
+    title: "QE Innovations",
     duration: "April 2022 - Jul 2023",
-    subtitle: "Software Engineer",
+    subtitle: "CNC Programmer",
     details: [
-      "Spearheaded the implementation of micro-frontends using Module Federation, enabling modular and scalable architecture.",
-      "Earned the Biofourmis Bravo Award for upgrading the application to support flexible operations across multiple geographies.",
-      "Enhanced performance and user experience by integrating Twilio-Video, Countly, and NGXS WebSocket for real-time data management.",
+      "SolidCAM programming okuma big and okuma small, programming robot, ordering and maintaining stock levels
     ],
-    tags: [
-      "JavaScript",
-      "Angular",
-      "RxJS",
-      "NGXS",
-      "TypeScript",
-      "RxState",
-      "Webpack",
-      "Optimization",
-    ],
+
     icon: "heartbeat",
   },
   {
-    title: "Novopay Solutions Private Limited",
+    title: "RØDE Microphones",
     duration: "June 2020 - April 2022",
-    subtitle: "Software Engineer",
+    subtitle: "CNC Machinist",
     details: [
-      "Built critical banking features including AePS services (eKYC, cash withdrawal, balance inquiry) used by over 1,00,000 retailers.",
-      "Developed QR code integration for wallet money loading, streamlining retailer workflows and reducing errors by 25 percent.",
-      "Integrated Clevertap analytics, Freshdesk ticketing, and chatbot services, enhancing user support and engagement.",
+      "Mastercam programming of CNC, wire-edm, mill-turn lathes
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
     icon: "qrcode",
   },
   {
-    title: "ThinkPedia LLP",
+    title: "ANSTO (The Australian Nuclear Science and Technology Organisation)",
     duration: "May 2019 - June 2019",
-    subtitle: "SDE Intern",
+    subtitle: "CNC Machininst / Apprentice",
     details: [
-      `Designed and developed a social media post management panel, improving collaboration efficiency by 50 percent.`,
+      "completed apprenticeship,
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
     icon: "group",
   },
 ];
 
-export const trekking = [
-  "<strong>Kheerganga Trek</strong>, Himachal Pradesh (9,711 feet)",
-  "<strong>Triund Trek</strong>, Himachal Pradesh (9,350 feet)",
-  "<strong>Kedarkantha Trek</strong>, Uttarakhand (12,500 feet)",
-  "<strong>Jalori Pass Trek</strong>, Himachal Pradesh (10,800 feet)",
-  "<strong>Vaishno Devi Trek</strong>, Jammu & Kashmir (5,200 feet)",
-];
 
 export const education = [
   {
-    title: "B.Tech. in Computer Science and Engineering",
+    title: "Uni - unfinished",
     duration: "2016 - 2020",
-    subtitle: "National Institute of Technology, Warangal",
+    subtitle: "University of Technology, Sydney",
     details: [],
     tags: [
-      "Data Structures & Algorithms",
-      "Operating Systems",
-      "Database Management System",
-      "Computer Networks",
-      "Compiler Designing",
-      "Cloud Computing",
     ],
     icon: "graduation-cap",
   },
   {
-    title: "Class 12th in Science and Mathematics",
+    title: "Ultimo TAFE",
     duration: "",
-    subtitle: "Board of Secondary Education, Rajasthan",
+    subtitle: "Certificate 4",
     details: [],
-    tags: ["Physics", "Chemistry", "Mathematics"],
+    icon: "book",
+  },
+    {
+    title: "Ultimo TAFE",
+    duration: "",
+    subtitle: "Certificate 3",
+    details: [],
     icon: "book",
   },
 ];
 
-export const footer = [
-  //   {
-  //     label: "Dev Profiles",
-  //     data: [
-  //       {
-  //         text: "Stackoverflow",
-  //         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
-  //       },
-  //       {
-  //         text: "GitHub",
-  //         link: "https://github.com/vinaysomawat",
-  //       },
-  //       {
-  //         text: "LeetCode",
-  //         link: "https://leetcode.com/somawatvinay/",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     label: "Resources",
-  //     data: [
-  //       {
-  //         text: "Enable Dark/Light Mode",
-  //         func: "enableDarkMode()",
-  //       },
-  //       {
-  //         text: "Print this page",
-  //         func: "window.print()",
-  //       },
-  //       {
-  //         text: "Clone this page",
-  //         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
-  //       },
-  //     ],
-  //   },
-  {
-    label: "Links",
-    data: [
-      //   {
-      //     text: "Linkedin",
-      //     link: "https://www.linkedin.com/in/vinaysomawat/",
-      //   },
-      //   {
-      //     text: "Twitter",
-      //     link: "https://twitter.com/thesigmakid",
-      //   },
-      {
-        text: "Buy me a coffee",
-        link: "https://www.buymeacoffee.com/r194dME8y",
-      },
-      {
-        text: "Github",
-        link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
-      },
-    ],
-  },
-  {
-    label: "copyright-text",
-    data: ["Made with &hearts; by Vinay Somawat"],
-  },
-];
+
