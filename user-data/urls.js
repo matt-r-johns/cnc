fd00d4ff7284c6a25e604d920ab7e3213b89f78c
matt-r-johns <mattjohns92@gmail.com>
2025-04-08 09:15:43 +0100

    1

--- user-data/urls.js
+++
@@ -1,7 +0,0 @@
-const githubUsername = "matt-r-johns";
-
-const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
-
-export const URLs = {
-    gitConnected: createGitConnectedURL(githubUsername),
-};
